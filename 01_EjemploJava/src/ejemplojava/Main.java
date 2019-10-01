8c0b85e580b280a5e98b38835c857e577f45a089
Sergio <sergio.fuenla92@gmail.com>
2019-10-01 17:56:00 +0200

    Update repositorio

--- 01_EjemploJava/src/ejemplojava/Main.java
+++ 01_EjemploJava/src/ejemplojava/Main.java
@@ -5,17 +5,71 @@
  */
 package ejemplojava;
 
+import clasesjava.ProbandoString;
+
 /**
  *
- * @author Formacion
+ * @author Admin
  */
 public class Main {
-
+    public static void main(String[] args) {
+        ProbandoString.tratarCadenas();
+    }
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void mainCoches(String[] args) {
+        
+        System.out.println("Empezando POO");
+        Coche miCoche;
+        
+        miCoche = new Coche("Kia", TipoCarburante.GASOLINA, "Rojo"); // La instanciacion se hace con new
+        
+        
+        miCoche.echarCarburante(30); // Empezamos con medio deposito
+        
+        System.out.println("Todos los coches tienen: " + Coche.CAPACIDAD_DEPOSITO);
+        System.out.println("Coche creado: " + miCoche.getMarca());
+        System.out.println("Nivel deposito: " + miCoche.getNivelDep() + " litros");
+        
+        
+        Coche otroCoche = miCoche; // new Coche();
+        // otroCoche.marca = "Seat"; YA NO SE PUEDE
+        System.out.println("Coche creado: " + otroCoche.getMarca());
+        // vaciarDeposito(miCoche);
+        miCoche.vaciarDeposito();
+        System.out.println("Nivel deposito: " + otroCoche.getNivelDep() + " litros");
+        
+        miCoche.setArrancado(true);
+        miCoche.echarCarburante(-2);
+        for (int i = 0; i < 3; i++) 
+            miCoche.acelerar();
+        
+        miCoche.mostrar();
+        
+        CocheRally miSupra = new CocheRally("TOYOTA", TipoCarburante.GASOLINA,
+                "ROJO", 2.1f);
+        miSupra.echarCarburante(40);
+        miSupra.setArrancado(true);
+        miSupra.acelerar();
+        miSupra.acelerar();
+        miSupra.derrapar();
+        miSupra.mostrar();
+        
+        Coche supraNormal = miSupra; //Conversion implicita
+        supraNormal.acelerar();
+        supraNormal.explosionCilindro();
+        //SupraNormal.derrapar();
+        supraNormal.acelerar();
+        supraNormal.mostrar();
+        
+        CocheRally cr = (CocheRally) supraNormal; //Conversion explicita o casting
+        System.out.println("Y el rozamiento??" + cr.getRozamiento());
+        
+        cr = (CocheRally) miCoche;
+        System.out.println("Y el rozamiento??" + cr.getRozamiento());
+        
+        
     }
-    
 }
